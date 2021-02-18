6bfbc189653b6c371540bb1a85a6b76ae67416eb
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-02-18 09:26:47 +0600

    so far so good

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_KEY = 'task.selectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_KEY,)
+
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -35,6 +44,9 @@ function render() {
         listElement.dataset.listId = list.id
         listElement.classList.add("list-name")
         listElement.innerText = list.name
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement)
     })
 }
